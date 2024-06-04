703d6d4073e08b98d109aebf68d909221ae5f80d
Gaurav237 <gktiwary237@gmail.com>
2024-06-04 19:09:35 +0530

    SinglePage details section added

--- frontend/src/routes/singlePage/SinglePage.jsx
+++ frontend/src/routes/singlePage/SinglePage.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import "./singlePage.scss";
+import Slider from "../../components/slider/Slider";
+import { singlePostData, userData } from "../../lib/dummydata";
 
 const SinglePage = () => {
   return (
-    <div>
-      <h1>Single Page</h1>
+    <div className="single-page">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="pin" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="user" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">
+              {singlePostData.description}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper"></div>
+      </div>
     </div>
   );
 };
