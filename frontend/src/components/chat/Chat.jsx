de80eee59b51e375cc7953e9805070897dbd2e45
Gaurav237 <gktiwary237@gmail.com>
2024-06-05 04:27:51 +0530

    ProfilePage - chatbox ui designed

--- frontend/src/components/chat/Chat.jsx
+++ frontend/src/components/chat/Chat.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.scss";
 
 const Chat = () => {
+  const [chat, setChat] = useState(true);
+
   return (
     <div className="chat">
       <div className="messages">
@@ -47,9 +49,72 @@ const Chat = () => {
           <p>message content, message content ...</p>
         </div>
       </div>
-      <div className="chat-box">
-        box
-      </div>
+      {chat !== null && (
+        <div className="chat-box">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              <span>John Cena</span>
+            </div>
+            <div className="close" onClick={() => setChat(null)}>
+              X
+            </div>
+          </div>
+          <div className="center">
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea name="" id=""></textarea>
+            <button>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
