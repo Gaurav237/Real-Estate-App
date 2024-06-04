03935c634b2101be9372b53f94485b67904f1d5e
Gaurav237 <gktiwary237@gmail.com>
2024-06-05 03:35:59 +0530

    profilePage - details section done

--- frontend/src/routes/profilePage/ProfilePage.jsx
+++ frontend/src/routes/profilePage/ProfilePage.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import "./profilePage.scss";
+import List from "../../components/list/List";
 
 const ProfilePage = () => {
   return (
     <div className="profile-page">
-      <h1>ProfilePage </h1>
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+          <div className="info">
+            <span>
+              Avatar:{" "}
+              <img
+                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+            </span>
+            <span>
+              Username: <b>John Cena</b>
+            </span>
+            <span>
+              Email: <b>john@gmail.com</b>
+            </span>
+          </div>
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+          <List />
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+          <List />
+        </div>
+      </div>
+      <div className="chat-container">
+        <div className="wrapper">
+
+        </div>
+      </div>
     </div>
   );
 };
