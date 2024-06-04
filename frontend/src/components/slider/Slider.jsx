a0f4a3d2ac70e9cc397015a23ad5c7a5e7611afb
Gaurav237 <gktiwary237@gmail.com>
2024-06-04 19:53:42 +0530

    slider component (half done)

--- frontend/src/components/slider/Slider.jsx
+++ frontend/src/components/slider/Slider.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./slider.scss";
 
-const Slider = () => {
+const Slider = ({ images }) => {
   return (
     <div className="slider">
-      <p>Slider</p>
+      <div className="big-image">
+        <img src={images[0]} />
+      </div>
+      <div className="small-images">
+        {images.slice(1).map((image, index) => (
+          <img src={image} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
