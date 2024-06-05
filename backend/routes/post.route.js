f0f9196743bc3eb71644aea8608e7fd0d99f32ec
Gaurav237 <gktiwary237@gmail.com>
2024-06-06 02:25:11 +0530

    auth : register route initialized

---
+++ backend/routes/post.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+// router.get("/test", (req, res) => {
+//   console.log("post router works");
+//   res.send("it works");
+// });
+
+export default router;
