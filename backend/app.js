7444a15a17783165a56cb6ebbbf9a48ef1628b85
Gaurav237 <gktiwary237@gmail.com>
2024-06-07 21:40:47 +0530

    register user completed

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,14 @@ import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+// Configure CORS with dynamic client URL and credentials support
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
